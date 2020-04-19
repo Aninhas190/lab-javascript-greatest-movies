7491227213b594b88275a430bf9b1de64bb2c76d
Ana de Almeida <ana.isabel190@gmail.com>
2020-04-19 19:22:34 +0100

    done

--- src/movies.js
+++ src/movies.js
@@ -9,25 +9,55 @@ const howManyMovies = (array) => array
     .filter((movie) => movie.director === 'Steven Spielberg' && movie.genre.includes('Drama'))
     .length;  
 //console.log(howManyMovies(movies));
-// Iteration 3: All rates average - Get the average of all rates with 2 decimals
-const ratesAverage = (array) => {
-    if (movies.length) {
-        const total = movies.reduce((acc, movie) => {
-            if (typeof movie.rate === 'number') {
-                acc += movie;    
-            }    
-        });
-    return total / movies.length;    
+// Iteration 3: All rates average - Get the average of all rates with 2 decimals - could not finish
+const ratesAverage = (array)  => {
+    if (array.length === 0) {
+      return 0;
+    }
+    const rates = array
+      .map((movie) => movie.rate)
+      // .filter(rate => typeof rate === 'number');
+      .map((rate) => {
+        if (typeof rate === 'number') {
+          return rate;
+        } else {
+          return 0;
+        }
+      });
+    const sumOfAllRates = rates.reduce((accumulator, rate) => accumulator + rate, 0);
+    const averageRate = sumOfAllRates / rates.length;
+
+    return Number(averageRate.toFixed(2));
+}
+//console.log(ratesAverage(movies));
+// Iteration 4: Drama movies - Get the average of Drama Movies
+const dramaMoviesRate = (array) => { 
+    const dramaMovies = array.filter((movie) => movie.genre.includes('Drama'));
+    if (!dramaMovies.length) return 0;
+    const averageRateDrama = (dramaMovies.reduce((acc, drama) => acc + drama.rate, 0))/ dramaMovies.length ; 
+    return Number(averageRateDrama.toFixed(2));
 };
 
-console.log(ratesAverage(movies))
-// Iteration 4: Drama movies - Get the average of Drama Movies
-const dramaMoviesRate = (array) => {};
+//console.log(dramaMoviesRate(movies));
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-const orderByYear = (array) => {};
+const orderByYear = (array) => {
+    const newArray = array.map((movie) => movie);
+    newArray.sort((a, b) => a.year > b.year ? 1 : -1);
+    return newArray;
+};
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-const orderAlphabetically = (array) => {};
+const orderAlphabetically = (array) => {
+    const newArray = array.map((movie) => movie.title);
+    newArray.sort((a, b) => a > b ? 1 : -1);
+    if (newArray.length > 20) {
+        return newArray.slice(0, 20);
+    }
+    return newArray;
+};
+
+console.log(orderAlphabetically(movies))
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 const turnHoursToMinutes = (array) => {};
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
-const bestYearAvg = (array) => {}
+const bestYearAvg = (array) => {};
